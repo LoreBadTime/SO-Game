1ea1840b11fe67bc52fa8c8e130adc1151f6bb18
Salvatore Castello <93254988+SalvatoreCastello@users.noreply.github.com>
2021-11-24 19:08:13 +0100

    Update sprites.c
    Minor update

--- sprites.c
+++ sprites.c
@@ -10,7 +10,7 @@
 typedef struct {
 	int x;
 	int y;
-	char c;
+	char* c[DIM-1];
 } oggetto;
 
 void main(){
@@ -18,38 +18,38 @@ void main(){
 	noecho();
 	curs_set(0);
 	int maxx =0, maxy = 0, i = 0;
-	char* sprite[DIM-1];
-	sprite[0]="|\\ ";
-	sprite[1]="| \\ ";
-	sprite[2]="|  |==";
-	sprite[3]="| /";
-	sprite[4]="|/";
+	oggetto sprite;
+	sprite.c[0]="|\\ ";
+	sprite.c[1]="| \\ ";
+	sprite.c[2]="|  |==";
+	sprite.c[3]="| /";
+	sprite.c[4]="|/";
 	char c;
 
 	getmaxyx(stdscr, maxy, maxx);
-	int x = 1, y = (maxy/2);
+	sprite.x = 1, sprite.y = (maxy/2);
 	
-	for(i=0;i<DIM;i++) mvprintw(y+i,x,sprite[i]);
+	for(i=0;i<DIM;i++) mvprintw(sprite.y+i,sprite.x,sprite.c[i]);
 	refresh();
 
 	do{
 		c=getch();
 		switch(c){
 			case SU:
-				if(y!=0){
-					y--;
+				if(sprite.y!=0){
+					sprite.y--;
 					clear();
 					for(i=0;i<DIM;i++)
-					mvprintw(y+i,x,sprite[i]);	
+					mvprintw(sprite.y+i,sprite.x,sprite.c[i]);	
 					refresh();
 				}
 				break;
 			case GIU:
-				if(y+5!=maxy){
-					y++;
+				if(sprite.y+5!=maxy){
+					sprite.y++;
 					clear();
 					for(i=0;i<DIM;i++)
-					mvprintw(y+i,x,sprite[i]);	
+					mvprintw(sprite.y+i,sprite.x,sprite.c[i]);	
 					refresh();
 				}
 				break;		
