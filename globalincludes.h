8ef68df470bd773559812f0da55714a367b96807
Salvatore Castello <93254988+SalvatoreCastello@users.noreply.github.com>
2021-12-08 21:07:29 +0100

    Update globalincludes.h
    Ho aggiunto le strutture e qualche MACRO all'interno di questo header

--- globalincludes.h
+++ globalincludes.h
@@ -11,4 +11,23 @@
 #include <signal.h>
 #include "./assets/animations.h"
 
+#define pos_X 0
+#define pos_Y 1
+#define SU 65 /* Freccia su */
+#define GIU 66 /* Freccia giu */
+#define DIM 5
+
+typedef struct {
+  int x;        // Coordinata x
+  int y;        // Coordinata y
+} Posizione;
+
+typedef struct {
+	int x;
+	int y;
+	char* c[DIM-1];
+} oggetto;
+
+void menu();
+
 #endif
