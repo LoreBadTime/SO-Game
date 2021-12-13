6f449c06b5457533b96f309027bb2af6ab95049d
Salvatore Castello <93254988+SalvatoreCastello@users.noreply.github.com>
2021-12-13 23:30:08 +0100

    Update process.h

--- versione_processi/process.h
+++ versione_processi/process.h
@@ -1,9 +1,11 @@
 #ifndef PROC_H
 #define PROC_H
 #include "../globalincludes.h"
-#define ENEM_TEST 5
-//con + di 8 nemici lo spawner attuale impazzisce,necessario algoritmo migliore
+
+#define ENEM_TEST 1 //con + di 8 nemici lo spawner attuale impazzisce,necessario algoritmo migliore
+
 int enemyLV1(int x,int y,int id,int direzione,int *sender,int *receiver);
 void *getinput();
 void screen(WINDOW *w1);
+
 #endif
