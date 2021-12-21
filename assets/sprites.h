d8236c11996d6e58d3fa0f4703571cded5365e58
Salvatore Castello <93254988+SalvatoreCastello@users.noreply.github.com>
2021-12-21 18:21:21 +0100

    Update sprites.h
    Definizioni frame 3 e 4 nemico lvl2

--- assets/sprites.h
+++ assets/sprites.h
@@ -9,6 +9,8 @@ void printnavetta_distrutta_f3(int x,int y,WINDOW *w);
 void printnavetta_distrutta_f4(int x,int y,WINDOW *w);
 void printnemicolv2_f1(int x,int y,WINDOW *w);
 void printnemicolv2_f2(int x,int y,WINDOW *w);
+void printnemicolv2_f3(int x,int y,WINDOW *w);
+void printnemicolv2_f4(int x,int y,WINDOW *w);
 void printnemicolv1_f1(int x,int y,WINDOW *w);
 void printnemicolv1_f2(int x,int y,WINDOW *w);
 void print_info(int proiettile_pronto, int life,WINDOW *w1,int maxx);
