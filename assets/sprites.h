111673ca2f92ed0ff6118e970da80d288dd11221
Salvatore Castello <93254988+SalvatoreCastello@users.noreply.github.com>
2021-12-26 22:44:12 +0100

    Aggiunta Menu e Grafiche varie

--- assets/sprites.h
+++ assets/sprites.h
@@ -15,5 +15,6 @@ void printnemicolv1_f1(int x,int y,WINDOW *w);
 void printnemicolv1_f2(int x,int y,WINDOW *w);
 void print_info(int proiettile_pronto, int life,WINDOW *w1,int maxx);
 int print_nave(int invincibility,WINDOW *w1,int x,int y);
-void main_test();
+void print_titolo(WINDOW* w,int x, int y);
+
 #endif
