3f6d1350f979c982e1790dbae138cf430424b603
Salvatore Castello <93254988+SalvatoreCastello@users.noreply.github.com>
2022-01-02 22:38:30 +0100

    Update threads.h

--- versione_threads/threads.h
+++ versione_threads/threads.h
@@ -1,9 +1,13 @@
-#ifndef THREADS_H
-#define THREADS_H
+#ifndef SO_GAME_MAIN_THREADS_H
+#define SO_GAME_MAIN_THREADS_H
+
 #include "../globalincludes.h"
 
 #define ENEM_TEST 30 //con + di 8 nemici lo spawner attuale impazzisce,necessario algoritmo migliore
 #define MAX_PROIETTILI 2
+int enemyLV1(int x,int y,int id,int direzione,int *sender,int *receiver);
+void *getinput();
 void screen_threads(WINDOW *w1, int num_nemici, int rimbalzi, int colore);
 
-#endif
+
+#endif //SO_GAME_MAIN_THREADS_H
