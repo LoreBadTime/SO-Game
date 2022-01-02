23b42ff57d052439769d3c23fcb89d14cfdbe028
Salvatore Castello <93254988+SalvatoreCastello@users.noreply.github.com>
2022-01-02 22:40:29 +0100

    Update globalincludes.h

--- globalincludes.h
+++ globalincludes.h
@@ -9,9 +9,11 @@
 #include <time.h>
 #include <pthread.h>
 #include <signal.h>
+#include <semaphore.h>
 #include "./assets/animations.h"
 #include "./assets/sprites.h"
 #include "./versione_processi/process.h"
+#include "./versione_threads/threads.h"
 
 #define pos_X 0
 #define pos_Y 1
