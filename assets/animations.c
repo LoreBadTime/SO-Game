130b16e8137911a62354106071e061f47bb19292
Salvatore Castello <93254988+SalvatoreCastello@users.noreply.github.com>
2022-01-03 19:32:51 +0100

    Commenti

--- assets/animations.c
+++ assets/animations.c
@@ -1,144 +1,132 @@
 #include "animations.h"
 #include "sprites.h"
 
-// Questa sezione di codice serve per le animazioni generali
-
-//NEL MAKEFILE INSERIRE QUESTA RIGA,OPPURE DIGITARE IL COMANDO NELLA SHELL:"export TERM=xterm-256color",abilitera la modifica e l'aggiunta di più colori
-
-/* game_over = Animazione di gameover.
- * int x prende le ascisse della navetta attuali(potrebbe non servire).
- * int y prende le ordinate della navetta attuali.*/
+/**
+ * Animazione di sconfitta ( Game Over )
+ * 
+ * WINDOW* w = Finestra di gioco.
+ * int x = Ascissa della nave.
+ * int y = Ordinata della nave.
+ */
 void game_over(WINDOW *w1,int x,int y){
-	//clear iniziale
-	wclear(w1);
 
-	const char string[] = "GAME OVER";
-	char tmp[2] = {' ', '\0'};
-	int i = 0;
+    /* Inizializzazione variabili */
+	const char string[] = "GAME OVER"; //Stringa di Game Over
+	int i; // Contatore
 
-	//Definizione di colori
-
-
-	//Animazione schermo che flasha
+    /* Animazione iniziale */
+    wclear(w1); // Pulizia iniziale dello schermo
 	for(i=0;i < GM_CYCLES;i++){
-		//Inizio frame sfondo giallo
-		printnavetta_distrutta_f1(x,y,w1);
-		wbkgd(w1,COLOR_PAIR(WHI_RED));
-        refresh();
+		printnavetta_distrutta_f1(x,y,w1); // Stampa frame navetta distrutta, frame 1
+		wbkgd(w1,COLOR_PAIR(WHI_RED)); // Sfondo: BIANCO/ROSSO
 		wrefresh(w1);
-		//Fine frame sfondo giallo
-		napms(GM_SPEED-50);
+		napms(GM_SPEED-50); // Delay per l'animazione
 
-		//Inizio frame sfondo rosso
-		printnavetta_distrutta_f2(x,y,w1);
-		wbkgd(w1,COLOR_PAIR(RED_YEL));
-        refresh();
+		printnavetta_distrutta_f2(x,y,w1); // Stampa frame navetta distrutta, frame 2
+		wbkgd(w1,COLOR_PAIR(RED_YEL)); // Sfondo: ROSSO/GIALLO
 		wrefresh(w1);
-		//Fine frame sfondo rosso
-		napms(GM_SPEED-50);
+		napms(GM_SPEED-50); // Delay per l'animazione
 	}
 	
-	//Inizio animazione morte lenta - Rosso chiaro
-	napms(GM_DEATH);
-	printnavetta_distrutta_f3(x,y,w1);
-	wbkgd(w1,COLOR_PAIR(WHI_LRED));
+	/* Inizio animazione morte lenta - Rosso chiaro */
+	napms(GM_DEATH); // Delay per l'animazione
+	printnavetta_distrutta_f3(x,y,w1); // Stampa frame navetta distrutta, frame 3
+	wbkgd(w1,COLOR_PAIR(WHI_LRED)); // Sfondo: BIANCO/ROSSO CHIARO
 	wrefresh(w1);
-	napms(GM_DEATH-GM_SPEED);
-     //Fine
 
-	//Inizio animazione morte lenta - Rosso scuro
-	printnavetta_distrutta_f4(x,y,w1);
-	wbkgd(w1,COLOR_PAIR(WHI_DRED));
+	/* Inizio animazione morte lenta - Rosso scuro */
+    napms(GM_DEATH-GM_SPEED); // Delay per l'animazione
+	printnavetta_distrutta_f4(x,y,w1); // Stampa frame navetta distrutta, frame 4
+	wbkgd(w1,COLOR_PAIR(WHI_DRED)); // Sfondo: BIANCO/ROSSO SCURO
 	wrefresh(w1);
-	napms(GM_DEATH-GM_SPEED);
-	wbkgd(w1,COLOR_PAIR(WHITE_BLACK));
-    wclear(w1);
+
+    /* Inizio animazione lenta - Sfondo nero */
+    napms(GM_DEATH-GM_SPEED); // Delay per l'animazione
+    wbkgd(w1,COLOR_PAIR(WHITE_BLACK)); // Sfondo: BIANCO:NERO
+    wclear(w1); // Pulizia schermo
 	wrefresh(w1);
-	napms(7*GM_SPEED);
-	//Fine
+	napms(7*GM_SPEED); // Delay per l'animazione
 
-	wclear(w1); 
-	wrefresh(w1); //Fine
+	sleep(2); // Delay per l'animazione
 
-	sleep(2);
-
-	// Stampa scritta GAME OVER.
+	/* Stampa scritta GAME OVER. */
 	for(i=0;i < GM_CYCLES_L-1;i++){
+        /* Stampa carattere per carattere la stringa GAME OVER */
 		mvwaddch(w1,LINES/2,(COLS/2+i)-5,string[i]);
 		wrefresh(w1);
-		napms(3*GM_SPEED);
+		napms(3*GM_SPEED); // Delay per l'animazione
         wclear(w1);
 	}
 	wclear(w1); 
 	wrefresh(w1);
-	napms(3*GM_SPEED);
+	napms(3*GM_SPEED); // Delay per l'animazione
 
-	//Stampa della stringa GAME OVER per intero
+	/* Stampa della stringa GAME OVER per intero */
 	mvwprintw(w1,LINES/2,(COLS/2)-5,string);
 	wrefresh(w1);
-	sleep(3);
+	sleep(3); // Delay per l'animazione
     
-    //Fine dell'esecuzione
+    /* Fine esecuzione */
 	wclear(w1); 
 	wrefresh(w1);
 }
 
+/**
+ * Animazione di vittoria ( Victory )
+ * 
+ * WINDOW* w = Finestra di gioco.
+ * int x = Ascissa della nave.
+ * int y = Ordinata della nave.
+ */
 void victory(WINDOW *w1,int x,int y){
-    const char string[] = "VICTORY";
-	int i, j;
-    int maxx, maxy;
-    getmaxyx(w1,maxy,maxx);
+    
+    /* Inizializzazione variabili */
+    const char string[] = "VICTORY"; // Stringa di vittoria
+	int i, j; // Contatori
+    int maxx, maxy; // Variabili per la risoluzione schermo
+    getmaxyx(w1,maxy,maxx); // FUnzione per la risoluzione schermo
 
+    /* Animazione iniziale */
     wclear(w1); //Avvio tutto pulendo lo schermo
-
-	//Animazione schermo che flasha
 	for(i=0,j=2;j < maxx/2;i++,j+=1){
-		//Inizio frame sfondo Verde
-        printnavetta(x+j,y,w1);
-        wbkgd(w1,COLOR_PAIR(WHI_GR));
+        printnavetta(x+j,y,w1); // Stampa frame della navetta
+        wbkgd(w1,COLOR_PAIR(WHI_GR)); // Sfondo: BIANCO/VERDE
 		wrefresh(w1);
-		//Fine frame sfondo Verde
-		napms(GM_SPEED/20);
+		napms(GM_SPEED/20); // Delay per l'animazione
 
-		//Inizio frame sfondo Blu
-		wbkgd(w1,COLOR_PAIR(WHI_BL));
+		wbkgd(w1,COLOR_PAIR(WHI_BL));  // Sfondo: BIANCO/BLU
 		wrefresh(w1);
-		//Fine frame sfondo Blu
-		napms(GM_SPEED/20);
-
-        wclear(w1); //Rinizia il ciclo e la navetta si sposta
-	}
-
-    x+=j-5; //Riprende le ultime coordinate
-	//Inizio animazione lenta - Verde chiaro
-	napms(GM_DEATH);
-	wbkgd(w1,COLOR_PAIR(WHI_LGR));
-	wrefresh(w1);
-    //Fine
-
-	//Inizio animazione lenta - Verde scuro
-    napms(GM_DEATH-GM_SPEED);
-	wbkgd(w1,COLOR_PAIR(WHI_DGR));
-	wrefresh(w1);
-    //Fine
-
-    //Inizio animazione lenta - Sfondo nero
-	napms(GM_DEATH-GM_SPEED);
-    wclear(w1); 
-	wbkgd(w1,COLOR_PAIR(WHITE_BLACK));
-    wrefresh(w1);
-	sleep(2);
-    wclear(w1);
-    //Fine
-
-	// Stampa scritta VICTORY.
-	for(i=0;i < GM_CYCLES_W-1;i++){
-		mvwaddch(w1,LINES/2,(COLS/2+i)-5,string[i]);
-		wrefresh(w1);
-		napms(2*GM_SPEED);
+		napms(GM_SPEED/20); // Delay per l'animazione
         wclear(w1);
 	}
-	napms(3*GM_SPEED);
+
+	/* Inizio animazione lenta - Verde chiaro */
+	napms(GM_DEATH); // Delay per l'animazione
+	wbkgd(w1,COLOR_PAIR(WHI_LGR));  // Sfondo: BIANCO/VERDE CHIARO
+	wrefresh(w1);
+    
+	/* Inizio animazione lenta - Verde scuro */
+    napms(GM_DEATH-GM_SPEED); // Delay per l'animazione
+	wbkgd(w1,COLOR_PAIR(WHI_DGR));  // Sfondo: BIANCO/VERDE SCURO
+	wrefresh(w1);
+
+    /* Inizio animazione lenta - Sfondo nero */
+	napms(GM_DEATH-GM_SPEED); // Delay per l'animazione
+	wbkgd(w1,COLOR_PAIR(WHITE_BLACK));  // Sfondo: BIANCO/NERO
+    wrefresh(w1);
+    
+    
+	sleep(2); // Delay per l'animazione
+    wclear(w1);
+
+	/* Stampa scritta VICTORY carattere per carattere */
+	for(i=0;i < GM_CYCLES_W-1;i++){
+		mvwaddch(w1,LINES/2,(COLS/2+i)-5,string[i]); // Stampa carattere
+		wrefresh(w1);
+		napms(2*GM_SPEED); // Delay per l'animazione
+        wclear(w1);
+	}
+	napms(3*GM_SPEED); // Delay per l'animazione
 
 	/* Stampa della stringa VICTORY per intero che va a dx e sx
 	 * Insieme a 4 navicelle nemiche che la circondano */
@@ -149,7 +137,7 @@ void victory(WINDOW *w1,int x,int y){
         printnemicolv1_f1(maxx / 3, 2*(maxy / 3), w1); //Navicella sx basso
         printnemicolv1_f1(2*(maxx / 3), 2*(maxy / 3), w1); //Navicella dx basso
         wrefresh(w1);
-        napms(3*GM_SPEED);
+        napms(3*GM_SPEED); // Delay per l'animazione
 
         wclear(w1);
         mvwprintw(w1,LINES/2,(COLS/2),string);
@@ -158,9 +146,8 @@ void victory(WINDOW *w1,int x,int y){
         printnemicolv1_f2(maxx / 3, 2*(maxy / 3), w1); //Navicella sx basso
         printnemicolv1_f2(2*(maxx / 3), 2*(maxy / 3), w1); //Navicella dx basso
         wrefresh(w1);
-        napms(3*GM_SPEED);
+        napms(3*GM_SPEED); // Delay per l'animazione
         
         wclear(w1);
     }
-    //Fine dell'esecuzione
 }
