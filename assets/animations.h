3e3ea0bd1b99b69dc75fbb62a78f62973c048f9d
Salvatore Castello <93254988+SalvatoreCastello@users.noreply.github.com>
2022-01-03 19:33:11 +0100

    Commenti

--- assets/animations.h
+++ assets/animations.h
@@ -2,18 +2,14 @@
 #define ANIMATIONS_H
 #include "../globalincludes.h"
 
-
+/* MACRO di delay per l'animazione */
 #define GM_SPEED 150
 #define GM_CYCLES 5
 #define GM_CYCLES_L 10
 #define GM_CYCLES_W 8
 #define GM_DEATH 1000
 
-//Sprite per la navetta (da creare)
-#define NAVE_OK "O"
-#define NAVE_ESPLOSA "X"
-
-void game_over(WINDOW *w1,int x,int y);
-void victory(WINDOW *w1,int x,int y);
+void game_over(WINDOW *w1,int x,int y); // Funzione di Game_Over
+void victory(WINDOW *w1,int x,int y); // Funzione di vittoria
 
 #endif
