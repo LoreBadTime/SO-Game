e72fbf87cc1f5b75b9655ea2bd597c697676abe4
Salvatore Castello <93254988+SalvatoreCastello@users.noreply.github.com>
2022-01-04 13:45:48 +0100

    Commenti e definizioni

--- versione_processi/process.h
+++ versione_processi/process.h
@@ -2,10 +2,15 @@
 #define PROC_H
 #include "../globalincludes.h"
 
-#define ENEM_TEST 30 //con + di 8 nemici lo spawner attuale impazzisce,necessario algoritmo migliore
-#define MAX_PROIETTILI 2
-int enemyLV1(int x,int y,int id,int direzione,int *sender,int *receiver);
-void *getinput();
+/* Definizione MACRO */
+#define ENEM_TEST 30 // Massimo numero di nemici giocabili
+#define MAX_PROIETTILI 2 // Massimo numero di proiettili sparati ( non modificabile )
+
+/* Funzioni versione processi */
+void proiettile(WINDOW* w,int x, int y, int direzione, int *pipe);
+void bomba(WINDOW* w,int x, int y, int id,int *pipe);
+void nave(int *pipe, int sys_slownes);
+void nemico(int x,int y,int id,int direzione,int *sender,int *receiver);
 void screen(WINDOW *w1, int num_nemici, int rimbalzi, int colore);
 
 #endif
